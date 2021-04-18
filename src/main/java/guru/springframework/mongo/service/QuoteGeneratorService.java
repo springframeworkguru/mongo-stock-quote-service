ee992e20e755ed4db1f4c59b031c49255d67a21b
John Thompson <john@springframework.guru>
2021-04-18 12:17:25 -0400

    adding gen service

---
+++ src/main/java/guru/springframework/mongo/service/QuoteGeneratorService.java
@@ -0,0 +1,13 @@
+package guru.springframework.mongo.service;
+
+
+
+import guru.springframework.mongo.model.Quote;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+public interface QuoteGeneratorService {
+
+    Flux<Quote> fetchQuoteStream(Duration period);
+}
