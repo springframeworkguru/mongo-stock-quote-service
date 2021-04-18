b4d244692d15cf74f97578ebc6a38e751d4a3088
John Thompson <john@springframework.guru>
2021-04-18 11:11:56 -0400

    initial project add

---
+++ src/main/java/guru/springframework/mongo/MongoStockQuoteServiceApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoStockQuoteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoStockQuoteServiceApplication.class, args);
+    }
+
+}
